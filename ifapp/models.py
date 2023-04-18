88d7af4b031914292100fb0229d34d777c625cf4
ntcababp <dihfahsihm@gmail.com>
2023-04-18 23:53:27 +0300

    command for uploading data into db

--- ifapp/models.py
+++ ifapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Inventory(models.Model):
     date = models.DateField()
     item_name = models.CharField(max_length=100)
-    quantity = models.IntegerField(blank=True, null=True)
+    quantity = models.IntegerField(default=0, blank=True, null=True)
     quantity_used = models.IntegerField(default=0, blank=True, null=True)
     remaining_stock = models.IntegerField(default=0, blank=True, null=True)
     # other fields as needed
